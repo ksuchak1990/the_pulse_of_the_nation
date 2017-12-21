1882153dafb93e19cc84e6b8f752049ef389d726
ksuchak1990 <ksuchak1990@yahoo.co.uk>
2017-12-21 12:46:52 +0000

    tabs -> spaces, i/o functions, data collection wrapper

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 import requests
 import csv
 import codecs
+import json
+import os
 
 # Functions
+# Get url source code, else catch errors
 def requestURL(inputString):
     if not isinstance(inputString, str):
         raise TypeError('inputString must be a string') 
@@ -12,6 +15,7 @@ def requestURL(inputString):
         raise Exception('Page request unsuccessful: status code {0}'.format(r.status_code))
     return(r.text)
 
+# Get a substring from an input, given a start string and an end string
 def restrict(inputString, startStr=None, endStr=None):
     if not isinstance(inputString, str):
         raise TypeError('inputString must be a string')
@@ -22,27 +26,48 @@ def restrict(inputString, startStr=None, endStr=None):
 
 # Get urls of csvs on survey website
 def getDataURLs():
-	baseURL = 'https://thepulseofthenation.com'
-	sourceCode = requestURL(inputString=baseURL)
-	downloadSection = restrict(inputString=sourceCode, startStr='<ul class="downloads">', endStr='</ul>')
-	downloadList = downloadSection.split('<li>')[1:]
-	linkList = [restrict(inputString=x, startStr='href="', endStr='" download>') for x in downloadList]
-	urlList = ['{0}{1}'.format(baseURL, link) for link in linkList]
-	return urlList
+    baseURL = 'https://thepulseofthenation.com'
+    sourceCode = requestURL(inputString=baseURL)
+    downloadSection = restrict(inputString=sourceCode, startStr='<ul class="downloads">', endStr='</ul>')
+    downloadList = downloadSection.split('<li>')[1:]
+    linkList = [restrict(inputString=x, startStr='href="', endStr='" download>') for x in downloadList]
+    urlList = ['{0}{1}'.format(baseURL, link) for link in linkList]
+    return urlList
 
 # Given a list of links, get the csv files and merge them into a list of dicts
-def collectData(urlList):
-	data = list()
-	for url in urlList:
-		item = requests.get(url)
-		content = codecs.iterdecode(item.iter_lines(), 'utf-8')
-		csvReader = csv.DictReader(content)
-		for record in csvReader:
-			data.append(record)
-	return data
+def downloadData(urlList):
+    data = list()
+    for url in urlList:
+        print('Downloading data from {0}'.format(url))
+        item = requests.get(url)
+        content = codecs.iterdecode(item.iter_lines(), 'utf-8')
+        csvReader = csv.DictReader(content)
+        for record in csvReader:
+            data.append(record)
+    return data
+
+# Read in json data file
+def pickUp(path):
+    with open(path) as infile:
+        item = json.load(infile)
+    return item
+
+# Write out json data file
+def putDown(item, path):
+    print('Writing to {0}'.format(path))
+    with open(path, 'w') as outfile:
+        json.dump(item, outfile)
+
+# Wrapper for collection and writing of data
+def collectData():
+  urlList = getDataURLs()
+  print('Found {0} URLs'.format(len(urlList)))
+  data = downloadData(urlList)
+  print('Collected {0} records'.format(len(data)))
+  dataPath = './output/collectedData.json'
+  putDown(data, dataPath)
 
 # Main
-urlList = getDataURLs()
-print(urlList)
-data = collectData(urlList)
-print(len(data))
+collectData()
+
+# Main 2 - 
